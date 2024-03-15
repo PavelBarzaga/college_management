c7129b2a675c83fc94dfcd9f1b7c4d1c3274dcc3
PavelBarzaga <ethan920713@gmail.com>
2024-03-15 10:53:18 -0400

    Added logic for edition and deletion of courses

--- apps/academic_management/urls.py
+++ apps/academic_management/urls.py
@@ -3,6 +3,10 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [path("", views.home)] + static(
-    settings.STATIC_URL, document_root=settings.STATIC_ROOT
-)
+urlpatterns = [
+    path("", views.home),
+    path("registerCourse/", views.registerCourse),
+    path("deleteCourse/<code>", views.deleteCourse),
+    path("editCourse/<code>", views.editCourse),
+    path("courseEdition/", views.courseEdition),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- apps/academic_management/views.py
+++ apps/academic_management/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Course
 
 
 def home(request):
     courses = Course.objects.all()
     return render(request, "course_management.html", {"courses": courses})
+
+
+def registerCourse(request):
+    code = request.POST["txtCode"]
+    name = request.POST["txtName"]
+    credits = request.POST["numCredits"]
+    course = Course.objects.create(code=code, name=name, credits=credits)
+    return redirect("/")
+
+
+def editCourse(request, code):
+    course = Course.objects.get(code=code)
+    return render(request, "courseEdition.html", {"course": course})
+
+
+def deleteCourse(request, code):
+    course = Course.objects.get(code=code)
+    course.delete()
+
+    return redirect("/")
+
+
+def courseEdition(request):
+    code = request.POST["txtCode"]
+    name = request.POST["txtName"]
+    credits = request.POST["numCredits"]
+
+    course = Course.objects.get(code=code)
+    course.name = name
+    course.credits = credits
+    course.save()
+
+    return redirect("/")
